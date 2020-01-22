4b79ac0ad4dfe02045f6588cce428bd5977a8fb2
Bruno DaSilva <bananaatomica@Brunos-MacBook-Air.local>
2020-01-21 22:38:38 -0600

    added onKeyPress event to work, next handle onScreenKeyboard

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Ciao Mundo');
+console.log("Ciao Mundo");
+
+let textOnKeyPress = document.getElementById("text-area");
+let textOnKeyPress2 = (document.getElementById(
+  "text-area"
+).onclick = function() {
+  displayKeyValue();
+});
+
+window.addEventListener("keydown", function(e) {
+  console.log(e);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const value = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!value) return;
+
+  textOnKeyPress.value = `${e.key}`;
+});
